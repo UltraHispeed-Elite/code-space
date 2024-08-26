295b2537b2af1db1a4ec4093a282c4b2942b1111
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-08-26 13:39:37 +0000

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import useState from 'react';
+import './App.css';
+
+function App() {
+  const [screen, setScreen] = useState(0);
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
